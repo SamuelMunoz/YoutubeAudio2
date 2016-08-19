d79e862c9e48bd22f8669f4f1338b284eb9126d8
Samuel Munoz <sdmunoz@gmail.com>
2016-08-19 11:59:49 -0500

    First commit

---
+++ YoutubeAudio2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace YoutubeAudio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnDescargar = new System.Windows.Forms.Button();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dirección";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(70, 6);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(416, 20);
+            this.txtDireccion.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(70, 32);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(353, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.Location = new System.Drawing.Point(367, 61);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(119, 23);
+            this.btnDescargar.TabIndex = 3;
+            this.btnDescargar.Text = "Descargar!";
+            this.btnDescargar.UseVisualStyleBackColor = true;
+            this.btnDescargar.Click += new System.EventHandler(this.btnDescargar_Click);
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(431, 37);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(36, 13);
+            this.lblPercentage.TabIndex = 4;
+            this.lblPercentage.Text = "0.00%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 92);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.btnDescargar);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YoutubeAudio2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnDescargar;
+        private System.Windows.Forms.Label lblPercentage;
+    }
+}
+
---
+++ YoutubeAudio2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YoutubeAudio2
+{
+    public partial class Form1 : Form
+    {
+        private string _filepath;
+        private string _videotitle;
+        private string _audiotitle;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDescargar_Click(object sender, EventArgs e)
+        {           
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            var videos = DownloadUrlResolver.GetDownloadUrls(txtDireccion.Text);
+            var video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == 360);
+            if (video.RequiresDecryption)
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            _filepath = Path.Combine(Application.StartupPath + "\\", video.Title + video.VideoExtension);
+            _videotitle = video.Title + video.VideoExtension;
+            _audiotitle = $"{video.Title}.mp3";
+            var downloader = new VideoDownloader(video, _filepath);
+            downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+            var trd = new Thread(() =>
+            {
+                downloader.Execute();
+                var ffmpeg = Application.StartupPath + "\\ffmpeg.exe";
+                var args = $"-i \"{_videotitle}\" \"{_audiotitle}\" -y";
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = ffmpeg,
+                        Arguments = args
+                    }
+                };
+                proc.Start();
+                proc.WaitForExit();
+                File.Delete($"{Application.StartupPath}\\{_videotitle}");
+                var savedialog = new SaveFileDialog {FileName = _audiotitle};
+                if (savedialog.ShowDialog() != DialogResult.OK) return;
+                var fi = new FileInfo(savedialog.FileName);
+                if (fi.Exists) fi.Delete();
+                File.Move($"{Application.StartupPath}\\{_audiotitle}", savedialog.FileName);
+                Invoke(new MethodInvoker(delegate
+                {
+                    txtDireccion.Text = "";
+                    progressBar.Value = 0;
+                    // ReSharper disable once LocalizableElement
+                    lblPercentage.Text = "0.00%";
+                }));
+            }) {IsBackground = true};
+            trd.SetApartmentState(ApartmentState.STA);
+            trd.Start();
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, ProgressEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                progressBar.Value = (int) e.ProgressPercentage;
+                lblPercentage.Text = $"{e.ProgressPercentage:0.##}%";
+                progressBar.Update();
+            }));
+        }
+    }
+}
